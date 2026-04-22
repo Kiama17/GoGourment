41ed36eece4c3ff24be09b6d7731ab95197fbbf0
Kiama17 <nyaruaichristine5096@gmail.com>
2026-04-22 09:04:44 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import DashboardScreen from "./src/screens/DashboardScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-
-const Stack = createStackNavigator();
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
